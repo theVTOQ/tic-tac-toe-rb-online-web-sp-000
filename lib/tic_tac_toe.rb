986919b8265d9038b1128957259a0b96bbacf70b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-01 18:42:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,10 +16,12 @@ end
 
 # Define your play method below
 def play(board)
-  turn_count = 0
-  until turn_count == 9
-    turn_count += 1
-    turn(board)
+  while(!over?(board))
+    turn_count = 0
+    until turn_count == 9
+      turn_count += 1
+      turn(board)
+    end
   end
 end
 
