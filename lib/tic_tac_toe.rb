a19f2492ebc8b3e479d8237b9bb3624c0b21eedc
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-01 18:31:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 WIN_COMBINATIONS = [
-  [0,1,2], 
+  [0,1,2],
   [3,4,5],
   [6,7,8],
   [0,3,6],
@@ -21,3 +21,48 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def won?(board)
+
+  WIN_COMBINATIONS.each do |win_combo|
+    pos_1 = board[win_combo[0]]
+    pos_2 = board[win_combo[1]]
+    pos_3 = board[win_combo[2]]
+
+    x_wins = pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
+    y_wins = pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
+
+    if x_wins || y_wins
+      return win_combo
+    else
+      false
+    end
+  end
+
+  return nil
+end
+
+def full?(board)
+  board.none? {|i| i == "   " || i == " " || i == "" || i == nil}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  #no need to call draw? method here- it would be redundant
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if board.empty?
+    return nil
+  end
+
+  winning_combo = won?(board)
+  if winning_combo != nil
+    return board[winning_combo[0]]
+  else
+    return nil
+  end
+end
